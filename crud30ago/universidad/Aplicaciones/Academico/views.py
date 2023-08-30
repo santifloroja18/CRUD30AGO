a4573577cbe1af81de592eb0dbf92e49402a3d13
santifloroja18 <133369865+santifloroja18@users.noreply.github.com>
2023-08-30 11:24:42 -0500

    Add files via upload

---
+++ crud30ago/universidad/Aplicaciones/Academico/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Curso
+# Create your views here.
+
+def home(request):
+    cursosListados = Curso.objects.all()
+    return render(request, "gestionCursos.html", { "cursos":cursosListados } )
